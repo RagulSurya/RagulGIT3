7be3b8178e65137e4ca7c73db26f0fa5e7a608ef
sabarihari <sabarihari97@gmail.com>
2019-09-20 19:43:09 +0530

    Finished

--- src/org/sample/test/SuriyA.java
+++ src/org/sample/test/SuriyA.java
@@ -4,5 +4,9 @@ public class SuriyA {
 	public static void main(String[] args) {
 	String Movie = "KAAPAAN";
 	System.out.println(Movie);
+	String r = "Movie Review is Ultimate ";
+	System.out.println(r);
+	String rr = "Rating of the movie is 7.7";
+	System.out.println(rr);
 	}
 }
